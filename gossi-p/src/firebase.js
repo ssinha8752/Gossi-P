8324e73859fbd2616ff6ef6d5979ed67b8289d1b
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-04-17 01:52:42 +0530

    [Shubham] Initializing with firebase

---
+++ gossi-p/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCbZW3ThSZDqVmRtBK9_FY9PM03Vj8sCBU",
+    authDomain: "gossi-p.firebaseapp.com",
+    projectId: "gossi-p",
+    storageBucket: "gossi-p.appspot.com",
+    messagingSenderId: "468108320644",
+    appId: "1:468108320644:web:acdd5a4a327d2e913fc7d1",
+    measurementId: "G-B4KQ2RQ2BY"
+  };
