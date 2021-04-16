0904b26c4f44f74ddce071589a8c8d2c72482ce4
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-04-17 01:58:18 +0530

    [Shubham] Adding Sidebar chat component of the application (empty)

---
+++ gossi-p/src/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './chat.css'
+/**
+* @author
+* @function Chat
+**/
+
+function Chat(){
+  return(
+    <div className="chat">Chat</div>
+   )
+
+ }
+
+export default Chat
