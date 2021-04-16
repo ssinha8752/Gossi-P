42c36b6da919e8bbe5f9d692a49476fa8eec6b79
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-04-17 01:55:14 +0530

    [Shubham] Adding sidebar component with header

---
+++ gossi-p/src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './sidebar.css'
+import {Avatar, IconButton} from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {SearchOutlined} from '@material-ui/icons'
+import SidebarChat from './SidebarChat';
+/**
+* @author
+* @function Sidebar
+**/
+
+function Sidebar (){
+  return(
+    <div className="sidebar">
+        <div className="sidebar__header">
+        <Avatar alt="Shubham" src=".././1" />
+            <div className="sidebar_headerRight">
+            <IconButton>
+                <DonutLargeIcon/>
+            </IconButton>
+            <IconButton>
+                <ChatIcon/>
+            </IconButton>
+            <IconButton>
+                <MoreVertIcon/>
+            </IconButton>
+            </div>
+        </div>
+        <div className="sidebar__search">
+            <div className="sidebar__searchContainer">
+                <SearchOutlined/>
+                <input placeholder="Search or start a new chat" type="text"/>
+            </div>
+        </div>
+        ADD NEW CHAT HERE
+        <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+        </div>
+    </div>
+   )
+
+ }
+
+export default Sidebar
